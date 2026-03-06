ae12cb132d5f28a2e44cf6ef090ffbebce7f4255
Junaid-bi <junaidsofi639@gmail.com>
2026-03-06 19:49:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,19 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
+function searchProducts(){
 
-updateCart();
+let input = document.getElementById("search").value.toLowerCase();
 
-function addToCart(name,price){
+let products = document.querySelectorAll(".product");
 
-cart.push({name,price});
+products.forEach(product=>{
 
-localStorage.setItem("cart",JSON.stringify(cart));
+let name = product.innerText.toLowerCase();
 
-updateCart();
-
-alert("Added to cart");
-
+if(name.includes(input)){
+product.style.display="block";
+}else{
+product.style.display="none";
 }
-
-function updateCart(){
-
-let count = cart.length;
-
-document.querySelectorAll("#cart-count").forEach(el=>{
-el.innerText = count;
-});
-
-let cartItems = document.getElementById("cart-items");
-
-if(cartItems){
-
-cartItems.innerHTML="";
-
-let total = 0;
-
-cart.forEach((item,index)=>{
-
-total += item.price;
-
-cartItems.innerHTML +=
-`<li>${item.name} - ₹${item.price}
-<button onclick="removeItem(${index})">Remove</button>
-</li>`;
 
 });
-
-document.getElementById("total").innerText = total;
-
-}
-
-}
-
-function removeItem(index){
-
-cart.splice(index,1);
-
-localStorage.setItem("cart",JSON.stringify(cart));
-
-updateCart();
-
-}
-
-function clearCart(){
-
-cart=[];
-
-localStorage.setItem("cart",JSON.stringify(cart));
-
-updateCart();
 
 }
